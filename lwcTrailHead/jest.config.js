1a37eddea9b59ec93c14e77571a2a8ec44e8c5c1
omar <omarpolania@gmail.com>
2021-07-03 14:13:09 -0500

    JEST-TEST
    https://trailhead.salesforce.com/content/learn/modules/test-lightning-web-components

--- lwcTrailHead/jest.config.js
+++ lwcTrailHead/jest.config.js
@@ -2,5 +2,12 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    moduleNameMapper: {
+      '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel',
+      '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton',
+      '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+      }
 };
+
+
